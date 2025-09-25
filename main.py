e34a141625a8cae18f8d74f42dcb67d562cf4468
2024021129-crypto <2024021129-crypto@users.noreply.github.com>
2025-09-25 22:56:01 +0530

    Your commit message

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import os
 from fastapi import FastAPI
 from pydantic import BaseModel
 import torch
@@ -29,11 +28,5 @@ def predict(request: TextRequest):
     return {
         "text": request.text,
         "predicted_class": int(predicted_class),
-        "confidence": predictions[0][predicted_class].item()
+        "confidence": predictions[0][predicted_class].item() # type: ignore
     }
-
-# Add this so Render binds to correct port
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
